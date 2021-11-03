bc53f39f275b895c3639aee7c980e5a188645225
balumalladi <baluchowdary8014@gmail.com>
2021-11-02 21:27:04 -0500

    changes

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.listen(PORT, () => {
 
 const db = require("../server/models");
 db.mongoose
-  .connect(db.url, {
+  .connect(process.env.MONGODB_URI || db.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -42,3 +42,7 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+  }
